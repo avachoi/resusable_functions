9fca46da3eb4a8c71c3f61fdf05b541dd18a6c8d
avachoi <alexbchoi@gmail.com>
2024-05-28 15:45:49 -0400

    commented out

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 // //Part 1: Thinking Functionally
 
-// // Take an array of numbers and return the sum.
-// let sum = (array) => {
-// 	return array.reduce((prev, curr) => prev + curr, 0);
-// };
-// console.log(sum([1, 2, 3]));
-// // Take an array of numbers and return the average.
-// let average = (array) => {
-// 	return array.reduce((prev, curr) => prev + curr, 0) / array.length;
-// };
-// console.log(average([1, 2, 3]));
-// // Take an array of strings and return the longest string.
-// let getLongestString = (array) => {
-// 	let sorted = array.sort((a, b) => a.length - b.length);
-// 	return sorted[sorted.length - 1];
-// };
-// console.log(getLongestString(["say", "hello", "morning", "in", "the"]));
-// // Take an array of strings, and a number and return an array of the strings that are longer than the given number.
-// // For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
-// let stringsLongerThanNum = (array, num) => {
-// 	return array.filter((el) => el.length > num);
-// };
-// console.log(stringsLongerThanNum(["say", "hello", "in", "the", "morning"], 3));
-// // Take a number, n, and print every number between 1 and n without using loops. Use recursion.
-// let numbersBetween = (n) => {
-// 	for (let i = 2; i < n; i++) {
-// 		console.log(i);
-// 	}
-// };
-// console.log(numbersBetween(5));
+// Take an array of numbers and return the sum.
+let sum = (array) => {
+	return array.reduce((prev, curr) => prev + curr, 0);
+};
+console.log(sum([1, 2, 3]));
+// Take an array of numbers and return the average.
+let average = (array) => {
+	return array.reduce((prev, curr) => prev + curr, 0) / array.length;
+};
+console.log(average([1, 2, 3]));
+// Take an array of strings and return the longest string.
+let getLongestString = (array) => {
+	let sorted = array.sort((a, b) => a.length - b.length);
+	return sorted[sorted.length - 1];
+};
+console.log(getLongestString(["say", "hello", "morning", "in", "the"]));
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number.
+// For example, stringsLongerThan(['say', 'hello', 'in', 'the', 'morning'], 3); would return ["hello", "morning"].
+let stringsLongerThanNum = (array, num) => {
+	return array.filter((el) => el.length > num);
+};
+console.log(stringsLongerThanNum(["say", "hello", "in", "the", "morning"], 3));
+// Take a number, n, and print every number between 1 and n without using loops. Use recursion.
+let numbersBetween = (n) => {
+	for (let i = 2; i < n; i++) {
+		console.log(i);
+	}
+};
+console.log(numbersBetween(5));
 
 //Part 2: Thinking Methodically
 let arr = [
